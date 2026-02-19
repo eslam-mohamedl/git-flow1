4a655b1f574e0e18798620f5109004e5cc4d4c9f
eslam-mohamedl <ee.eslam355@gmail.com>
2026-02-20 00:02:44 +0200

    feat(organisms): add blog section component

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,9 +1,10 @@
 import HeroSection from "@/components/organisms/HeroSection";
-
+import BlogSection from "@/components/organisms/BlogSection";
 export default function page() {
   return (
     <div>
       <HeroSection />
+      <BlogSection />
     </div>
   );
 }
